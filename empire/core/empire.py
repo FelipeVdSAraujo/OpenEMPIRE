bfdc3909671282fdad06d17b12e7a39089d2d1e2
Felipe van de Sande Araujo <felipe.van.de.s.araujo@ntnu.no>
2026-03-04 11:01:10 +0100

    fix - bug when running with iamc output

--- empire/core/empire.py
+++ empire/core/empire.py
@@ -1314,7 +1314,7 @@ def run_empire(name, tab_file_path: Path, result_file_path: Path, scenario_data_
         seasonhours=[]
     
         for s in instance.Season:
-            if s not in 'peak':
+            if 'peak' not in s:
                 t=pd.to_datetime(list(range(lengthRegSeason)), unit='h', origin=pd.Timestamp(seasonstart[s]))
                 t=[str(i)[5:-3] for i in t]
                 t=[str(i)+"+01:00" for i in t]
@@ -1363,23 +1363,28 @@ def run_empire(name, tab_file_path: Path, result_file_path: Path, scenario_data_
             f = row_write(f, "Europe", "Secondary Energy|Electricity", "EJ/yr", "Year", \
                     [value(sum(EJperMWh*instance.seasScale[s]*instance.genOperational[n,g,h,i,w] for (n,g) in instance.GeneratorsOfNode for (s,h) in instance.HoursOfSeason)) for i in instance.PeriodActive], Scenario+"|"+str(w)) #Total European generation per scenario
             for g in instance.Generator:
-                f = row_write(f, "Europe", "Active Power|Electricity|"+dict_generators[str(g)], "MWh", "Year", \
+                gen_iamc = dict_generators.get(str(g), str(g))
+                f = row_write(f, "Europe", "Active Power|Electricity|"+gen_iamc, "MWh", "Year", \
                     [value(sum(instance.seasScale[s]*instance.genOperational[n,g,h,i,w] for n in instance.Node if (n,g) in instance.GeneratorsOfNode for (s,h) in instance.HoursOfSeason)) for i in instance.PeriodActive], Scenario+"|"+str(w)) #Total generation per type and scenario
             for (s,h) in instance.HoursOfSeason:
                 for n in instance.Node:
-                    f = row_write(f, dict_countries_reversed[str(n)], "Price|Secondary Energy|Electricity", "US$2010/GJ", seasonhours[h-1], \
+                    node_region = dict_countries_reversed.get(str(n), str(n))
+                    f = row_write(f, node_region, "Price|Secondary Energy|Electricity", "US$2010/GJ", seasonhours[h-1], \
                         [value(instance.dual[instance.FlowBalance[n,h,i,w]]/(GJperMWh*instance.operationalDiscountrate*instance.seasScale[s]*instance.sceProbab[w])) for i in instance.PeriodActive], Scenario+"|"+str(w)+str(s))
         for g in instance.Generator:
-            f = row_write(f, "Europe", "Capacity|Electricity|"+dict_generators[str(g)], "GW", "Year", [value(sum(instance.genInstalledCap[n,g,i]*GWperMW for n in instance.Node if (n,g) in instance.GeneratorsOfNode)) for i in instance.PeriodActive]) #Total European installed generator capacity per type
-            f = row_write(f, "Europe", "Capital Cost|Electricity|"+dict_generators[str(g)], "US$2010/kW", "Year", [value(instance.genCapitalCost[g,i]*USD10perEUR18) for i in instance.PeriodActive]) #Capital generator cost
+            gen_iamc = dict_generators.get(str(g), str(g))  # fallback to raw name if unmapped
+            f = row_write(f, "Europe", "Capacity|Electricity|"+gen_iamc, "GW", "Year", [value(sum(instance.genInstalledCap[n,g,i]*GWperMW for n in instance.Node if (n,g) in instance.GeneratorsOfNode)) for i in instance.PeriodActive]) #Total European installed generator capacity per type
+            f = row_write(f, "Europe", "Capital Cost|Electricity|"+gen_iamc, "US$2010/kW", "Year", [value(instance.genCapitalCost[g,i]*USD10perEUR18) for i in instance.PeriodActive]) #Capital generator cost
             if value(instance.genMargCost[g,instance.PeriodActive[1]]) != 0: 
-                f = row_write(f, "Europe", "Variable Cost|Electricity|"+dict_generators[str(g)], "EUR/MWh", "Year", [value(instance.genMargCost[g,i]) for i in instance.PeriodActive])
-            f = row_write(f, "Europe", "Investment|Energy Supply|Electricity|"+dict_generators[str(g)], "billion US$2010/yr", "Year", [value((1/instance.LeapYearsInvestment)*USD10perEUR18* \
+                f = row_write(f, "Europe", "Variable Cost|Electricity|"+gen_iamc, "EUR/MWh", "Year", [value(instance.genMargCost[g,i]) for i in instance.PeriodActive])
+            f = row_write(f, "Europe", "Investment|Energy Supply|Electricity|"+gen_iamc, "billion US$2010/yr", "Year", [value((1/instance.LeapYearsInvestment)*USD10perEUR18* \
                     sum(instance.genInvCost[g,i]*instance.genInvCap[n,g,i] for n in instance.Node if (n,g) in instance.GeneratorsOfNode)) for i in instance.PeriodActive]) #Total generator investment cost per type
             if value(instance.genCO2TypeFactor[g]) != 0:
-                f = row_write(f, "Europe", "CO2 Emmissions|Electricity|"+dict_generators[str(g)], "tons/MWh", "Year", [value(instance.genCO2TypeFactor[g]*(GJperMWh/instance.genEfficiency[g,i])) for i in instance.PeriodActive]) #CO2 factor per generator type
+                f = row_write(f, "Europe", "CO2 Emissions|Electricity|"+gen_iamc, "tons/MWh", "Year", [value(instance.genCO2TypeFactor[g]*(GJperMWh/instance.genEfficiency[g,i])) for i in instance.PeriodActive]) #CO2 factor per generator type
         for (n,g) in instance.GeneratorsOfNode:
-            f = row_write(f, dict_countries_reversed[str(n)], "Capacity|Electricity|"+dict_generators[str(g)], "GW", "Year", [value(instance.genInstalledCap[n,g,i]*GWperMW) for i in instance.PeriodActive]) #Installed generator capacity per country and type
+            gen_iamc = dict_generators.get(str(g), str(g))
+            node_region = dict_countries_reversed.get(str(n), str(n))
+            f = row_write(f, node_region, "Capacity|Electricity|"+gen_iamc, "GW", "Year", [value(instance.genInstalledCap[n,g,i]*GWperMW) for i in instance.PeriodActive]) #Installed generator capacity per country and type
         
         f = f.groupby(['model','scenario','region','variable','unit','subannual']).sum().reset_index() #NB! DOES NOT WORK FOR UNIT COSTS; SHOULD BE FIXED
         
